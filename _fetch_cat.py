140f58d80e442fe271801f8b96147e45873b3fac
airealimboften <airealimboften@users.noreply.github.com>
2026-05-16 13:21:56 +0800

    chore: remove temp file

--- _fetch_cat.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import urllib.request, json
-url = "https://api.github.com/repos/airealimboften/knowledgebuilding/discussions/categories"
-req = urllib.request.Request(url, headers={"Accept": "application/vnd.github+json", "X-GitHub-Api-Version": "2022-11-28"})
-resp = urllib.request.urlopen(req, timeout=15)
-data = json.loads(resp.read().decode("utf-8"))
-for cat in data:
-    print(cat["name"], " | node_id:", cat.get("node_id", "N/A"))
